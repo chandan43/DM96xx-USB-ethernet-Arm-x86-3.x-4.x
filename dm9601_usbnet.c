f571d635edfc32775e87c8dc52accf10bcc929bf
Chandan <jha2011eec43@gmail.com>
2018-05-11 21:30:03 +0530

    Added rx_complete and rx_process fun

--- dm9601_usbnet.c
+++ dm9601_usbnet.c
@@ -13,6 +13,148 @@
 #include <linux/pm_runtime.h>
 
 /*-------------------------------------------------------------------------*/
+
+static inline void rx_process (struct usbnet *dev, struct sk_buff *skb)
+{
+	if (dev->driver_info->rx_fixup &&
+	    !dev->driver_info->rx_fixup (dev, skb)) {
+		/* With RX_ASSEMBLE, rx_fixup() must update counters */
+		if (!(dev->driver_info->flags & FLAG_RX_ASSEMBLE))
+			dev->net->stats.rx_errors++;
+		goto done;
+	}
+	// else network stack removes extra byte if we forced a short packet
+	
+	if (skb->len) {
+		/* all data was already cloned from skb inside the driver */
+		if (dev->driver_info->flags & FLAG_MULTI_PACKET)
+			dev_kfree_skb_any(skb);
+		else
+			usbnet_skb_return(dev, skb);
+		return;
+	}
+	netif_dbg(dev, rx_err, dev->net, "drop\n");
+	dev->net->stats.rx_errors++;
+done:
+	skb_queue_tail(&dev->done, skb);			
+}
+/*-------------------------------------------------------------------------*/
+
+/**
+ * mod_timer - modify a timer's timeout
+ * @timer: the timer to be modified
+ * @expires: new timeout in jiffies
+ *
+ * mod_timer() is a more efficient way to update the expire field of an
+ * active timer (if the timer is inactive it will be activated)
+ */
+/**
+ * timer_pending - is a timer pending?
+ * @timer: the timer in question
+ *
+ * timer_pending will tell whether a given timer is currently pending,
+ * or not. Callers must ensure serialization wrt. other operations done
+ * to this timer, eg. interrupt contexts, or other CPUs on SMP.
+ *
+ * return value: 1 if the timer is pending, 0 if not.
+ */
+static void rx_complete (struct urb *urb)
+{
+	
+	struct sk_buff		*skb = (struct sk_buff *) urb->context;
+	struct skb_data		*entry = (struct skb_data *) skb->cb;
+	struct usbnet		*dev = entry->dev;
+	int			urb_status = urb->status;
+	enum skb_state		state;
+
+	skb_put (skb, urb->actual_length);
+	state = rx_done;
+	entry->urb = NULL;
+
+	switch (urb_status) {
+	/* success */
+		case 0:
+			if (skb->len < dev->net->hard_header_len) {
+				state = rx_cleanup;
+				dev->net->stats.rx_errors++;
+				dev->net->stats.rx_length_errors++;
+				netif_dbg(dev, rx_err, dev->net,
+				  	"rx length %d\n", skb->len);
+			}
+			break;
+		/* stalls need manual reset. this is rare ... except that
+	 	* when going through USB 2.0 TTs, unplug appears this way.
+	 	* we avoid the highspeed version of the ETIMEDOUT/EILSEQ
+	 	* storm, recovering as needed.
+	 	*/
+		case -EPIPE:
+			dev->net->stats.rx_errors++;
+			usbnet_defer_kevent (dev, EVENT_RX_HALT);
+			// FALLTHROUGH
+		case -ECONNRESET:		/* async unlink */
+		case -ESHUTDOWN:		/* hardware gone */
+			netif_dbg(dev, ifdown, dev->net,
+			 	 "rx shutdown, code %d\n", urb_status);
+			goto block;
+
+		/* we get controller i/o faults during khubd disconnect() delays.
+	 	* throttle down resubmits, to avoid log floods; just temporarily,
+	 	* so we still recover when the fault isn't a khubd delay.
+	 	*/
+		case -EPROTO:
+		case -ETIME:
+		case -EILSEQ:	/* Illegal byte sequence */
+			dev->net->stats.rx_errors++;
+			if (!timer_pending (&dev->delay)) {
+				mod_timer (&dev->delay, jiffies + THROTTLE_JIFFIES);
+				netif_dbg(dev, link, dev->net,
+				  	"rx throttle %d\n", urb_status);
+			}
+block:
+	state = rx_cleanup;
+	entry->urb = urb;
+	urb = NULL;
+	break;
+
+	/* data overrun ... flush fifo? */
+	case -EOVERFLOW:
+		dev->net->stats.rx_over_errors++;
+		// FALLTHROUGH
+
+	default:
+		state = rx_cleanup;
+		dev->net->stats.rx_errors++;
+		netif_dbg(dev, rx_err, dev->net, "rx status %d\n", urb_status);
+		break;
+
+	}
+	/* stop rx if packet error rate is high */
+	if (++dev->pkt_cnt > 30) {
+		dev->pkt_cnt = 0;
+		dev->pkt_err = 0;
+	} else {
+		if (state == rx_cleanup)
+			dev->pkt_err++;
+		if (dev->pkt_err > 20)
+			set_bit(EVENT_RX_KILL, &dev->flags);
+	}
+	state = defer_bh(dev, skb, &dev->rxq, state);
+
+	if (urb) {
+		if (netif_running (dev->net) &&
+		    !test_bit (EVENT_RX_HALT, &dev->flags) &&
+		    state != unlink_start) {
+			rx_submit (dev, urb, GFP_ATOMIC);
+			usb_mark_last_busy(dev->udev);
+			return;
+		}
+		usb_free_urb (urb);
+	}
+	netif_dbg(dev, rx_err, dev->net, "no read resubmitted\n");
+
+}
+
+/*-------------------------------------------------------------------------*/
 void usbnet_pause_rx(struct usbnet *dev)
 {
 	set_bit(EVENT_RX_PAUSED, &dev->flags);
